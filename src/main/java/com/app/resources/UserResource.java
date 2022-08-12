882fa06b77c699b178d26a30425fc6a5e02e750a
Mauri Gonçalves <maurigvs@icloud.com>
2022-08-12 17:14:44 -0300

    User and UserResource

--- src/main/java/com/app/resources/UserResource.java
+++ src/main/java/com/app/resources/UserResource.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value="/users")
+@RequestMapping(value = "/users")
 public class UserResource {
 
     @GetMapping
