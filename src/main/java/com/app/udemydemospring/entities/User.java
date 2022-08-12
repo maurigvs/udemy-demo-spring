6da6a41696ad64ed1a245d552d74cec8e7d87f94
Mauri Gonçalves <maurigvs@icloud.com>
2022-08-12 18:41:25 -0300

    Order Entity

--- src/main/java/com/app/udemydemospring/entities/User.java
+++ src/main/java/com/app/udemydemospring/entities/User.java
@@ -2,14 +2,13 @@ package com.app.udemydemospring.entities;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
 @Table(name = "Users")
 public class User implements Serializable {
-
-    private static final long serialVersionUID = 1L;
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -17,6 +16,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -67,6 +69,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
