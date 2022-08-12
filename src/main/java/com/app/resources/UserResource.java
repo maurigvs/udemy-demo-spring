15145ad2f402b2b1e873e18fc1736c243cef30fb
Mauri Gonçalves <maurigvs@icloud.com>
2022-08-12 17:04:42 -0300

    User and UserResource

---
+++ src/main/java/com/app/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.app.resources;
+
+import com.app.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+
+        User user = new User(1L, "Mauri", "mauri@gmail.com", "+5511973722818", "123456");
+
+        return ResponseEntity.ok().body(user);
+    }
+}
