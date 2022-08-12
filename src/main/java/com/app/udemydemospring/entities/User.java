2330d3b4780064502c2505a3fc72d7be98c65739
Mauri Gonçalves <maurigvs@icloud.com>
2022-08-12 18:01:27 -0300

    Table name change

--- src/main/java/com/app/udemydemospring/entities/User.java
+++ src/main/java/com/app/udemydemospring/entities/User.java
@@ -1,11 +1,17 @@
 package com.app.udemydemospring.entities;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "Users")
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
