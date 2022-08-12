f48ac335964d9075e34618b18be5938ad8b13ab4
Mauri Gonçalves <maurigvs@icloud.com>
2022-08-12 16:53:24 -0300

    Initial commit

---
+++ src/main/java/com/app/udemydemospring/UdemyDemoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.app.udemydemospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyDemoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyDemoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/udemydemospring/UdemyDemoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.udemydemospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyDemoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
