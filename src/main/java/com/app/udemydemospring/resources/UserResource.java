25d0ee26306b0e90cfff462fe5284221ee4f6430
Mauri Gonçalves <maurigvs@icloud.com>
2022-08-12 18:29:32 -0300

    Service Layer

--- src/main/java/com/app/udemydemospring/resources/UserResource.java
+++ src/main/java/com/app/udemydemospring/resources/UserResource.java
@@ -1,19 +1,34 @@
 package com.app.udemydemospring.resources;
 
 import com.app.udemydemospring.entities.User;
+import com.app.udemydemospring.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
-    public ResponseEntity<User> findAll(){
+    public ResponseEntity<List<User>> findAll(){
 
-        User user = new User(1L, "Mauri", "mauri@gmail.com", "+5511973722818", "123456");
+        List<User> result = userService.findAll();
+        return ResponseEntity.ok().body(result);
+    }
 
+    @GetMapping(value="/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+
+        User user = userService.findById(id);
         return ResponseEntity.ok().body(user);
     }
 }
