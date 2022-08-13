16135abf6c89a357c97cdc6d97fee09eca6789bc
Mauri Gonçalves <maurigvs@icloud.com>
2022-08-13 18:34:54 -0300

    OrderItem entity

---
+++ src/main/java/com/app/udemydemospring/entities/pk/OrderItemPK.java
@@ -0,0 +1,52 @@
+package com.app.udemydemospring.entities.pk;
+
+import com.app.udemydemospring.entities.Order;
+import com.app.udemydemospring.entities.Product;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+import java.util.Objects;
+
+//Classe auxiliar para representar a associação muitos-para-muitos
+@Embeddable
+public class OrderItemPK implements Serializable {
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof OrderItemPK)) return false;
+        OrderItemPK that = (OrderItemPK) o;
+        return Objects.equals(getOrder(), that.getOrder()) && Objects.equals(getProduct(), that.getProduct());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getOrder(), getProduct());
+    }
+}
